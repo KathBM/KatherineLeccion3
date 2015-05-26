7a248819f7d488838d12df83774f15c9a1d96b7c
KathBM <kathbmcr@hotmail.com>
2015-05-25 21:06:33 -0600

    Lec03

---
+++ ConsoleApplication1/ConsoleApplication1/Fibonacci.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+
+
+    class Fibonacci
+    {
+
+        public void operacion()
+        {
+            ArrayList myAL = new ArrayList();
+            myAL.Add(4);
+            myAL.Add(6);
+            myAL.Add(20);
+            myAL.Add(1);
+            myAL.Add(11);
+            myAL.Add(25);
+        }
+        
+        foreach ( Object obj in myList ){
+
+           do (== 0)
+                return 0;
+
+            while (F1 == 1)
+                return 1;
+
+            return operacion(F1 - 1) + operacion(F1 - 2);
+         
+    }
+    }
+
+
+
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+
+        {
+            Fibonacci fibo = new Fibonacci();
+            fibo.operacion(2);
+            Console.WriteLine(fibo.operacion(4));
+            Console.WriteLine(fibo.operacion(6));
+            Console.WriteLine(fibo.operacion(20));
+            Console.WriteLine(fibo.operacion(1));
+            Console.WriteLine(fibo.operacion(11));
+            Console.WriteLine(fibo.operacion(25));
+          
+
+            System.Console.ReadLine();
+
+
+           
+        }
+    }
+}
+   
